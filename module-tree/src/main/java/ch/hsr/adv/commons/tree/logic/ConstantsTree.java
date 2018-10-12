482d2f7de832f87e18c0ebe1397065712754b36e
Fabian Meier <reiem_naibaf@hotmail.com>
2018-10-12 14:04:16 +0200

    added base elements for tree-module

---
+++ module-tree/src/main/java/ch/hsr/adv/commons/tree/logic/ConstantsTree.java
@@ -0,0 +1,12 @@
+package ch.hsr.adv.commons.tree.logic;
+
+/**
+ * Graph module-specific constants
+ */
+public class ConstantsTree {
+
+    /**
+     * Binary tree Module name
+     */
+    public static final String MODULE_NAME_BINARY_TREE = "tree-binary";
+}
---
+++ module-tree/src/main/java/ch/hsr/adv/commons/tree/logic/domain/ADVTreeNode.java
@@ -0,0 +1,25 @@
+package ch.hsr.adv.commons.tree.logic.domain;
+
+import ch.hsr.adv.commons.core.logic.domain.styles.ADVStyle;
+
+/**
+ * A TreeNode holds a value.
+ *
+ * @param <T> the value of the TreeNode
+ */
+interface ADVTreeNode<T> {
+
+    /**
+     * Returns the style for to the element
+     *
+     * @return style
+     */
+    ADVStyle getStyle();
+
+    /**
+     * Returns the content of the element
+     *
+     * @return content object
+     */
+    T getContent();
+}
