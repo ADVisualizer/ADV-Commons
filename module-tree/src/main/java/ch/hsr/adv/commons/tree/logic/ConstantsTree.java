2fdce3cc470e096a924a62df408cb87b3b2d7251
Fabian Meier <reiem_naibaf@hotmail.com>
2018-10-15 16:56:28 +0200

    added constant for array-indices-flag

--- module-tree/src/main/java/ch/hsr/adv/commons/tree/logic/ConstantsTree.java
+++ module-tree/src/main/java/ch/hsr/adv/commons/tree/logic/ConstantsTree.java
@@ -1,7 +1,7 @@
 package ch.hsr.adv.commons.tree.logic;
 
 /**
- * Graph module-specific constants
+ * Tree module-specific constants
  */
 public class ConstantsTree {
 
@@ -9,4 +9,9 @@ public class ConstantsTree {
      * Binary tree Module name
      */
     public static final String MODULE_NAME_BINARY_TREE = "tree-binary";
+
+    /**
+     * Flag for binary-trees to show array-indices
+     */
+    public static final String SHOW_ARRAY_INDICES = "show-array-indices";
 }
