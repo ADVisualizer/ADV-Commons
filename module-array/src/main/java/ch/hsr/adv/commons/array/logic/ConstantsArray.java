b4407edd26679c02596cc9130276f5a687fa48fc
Fabian Meier <reiem_naibaf@hotmail.com>
2018-11-19 16:32:51 +0100

    added constant show-array-indices in module-array

--- module-array/src/main/java/ch/hsr/adv/commons/array/logic/ConstantsArray.java
+++ module-array/src/main/java/ch/hsr/adv/commons/array/logic/ConstantsArray.java
@@ -9,4 +9,9 @@ public class ConstantsArray {
      * Module name
      */
     public static final String MODULE_NAME = "array";
+
+    /**
+     * Flag for binary-trees to show array-indices
+     */
+    public static final String SHOW_ARRAY_INDICES = "show-array-indices";
 }
