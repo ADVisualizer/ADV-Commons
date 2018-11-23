e44995fd40d393a0c79c78325bc4e30182d8bdda
Fabian Meier <reiem_naibaf@hotmail.com>
2018-11-23 10:20:34 +0100

    added constant keys for fixed nodes meta data

--- module-tree/src/main/java/ch/hsr/adv/commons/tree/logic/ConstantsTree.java
+++ module-tree/src/main/java/ch/hsr/adv/commons/tree/logic/ConstantsTree.java
@@ -30,4 +30,18 @@ public class ConstantsTree {
      * Flag for binary-trees to show array-indices
      */
     public static final String SHOW_ARRAY_INDICES = "show-array-indices";
+
+    /**
+     * this key is used for the binary tree modules when the client wants fixed
+     * nodes to set the max left height of a tree in the moduleGroup metadata
+     * it is needed because of the special circumstance of a left hanging tree
+     */
+    public static final String MAX_TREE_HEIGHT_LEFT = "max-left-tree-height";
+
+    /**
+     * this key is used for the binary tree modules when the client wants fixed
+     * nodes to set the max right height of a tree in the moduleGroup metadata
+     * it is needed because of the special circumstance of a right hanging tree
+     */
+    public static final String MAX_TREE_HEIGHT_RIGHT = "max-right-tree-height";
 }
